89eb330fcf8822588e0e36667e2af73dab1edbb4
Manimala Tangudu <35646962+manimalatangudu@users.noreply.github.com>
2023-01-22 16:37:45 -0500

    With dynamic input

--- src/MyComponent/MyComponent.js
+++ src/MyComponent/MyComponent.js
@@ -1,13 +1,23 @@
 import './MyComponent.css';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useEffect, useState } from 'react';
 
 function MyComponent() {
+    const [val, setVal] = useState(100);
+    useEffect(() => {
+        const myInterval = setTimeout(()=>{
+            const randomNum = Math.floor(Math.random()*3520);
+            console.log('RandomNum between the range-', randomNum);
+            setVal(randomNum);
+        }, 3000)
+        return () => clearTimeout(myInterval);
+    }, [val]);
   return (
     <div className="comp">
         <span className="state">Current</span>
         <CircularProgressbar
-          value={640}
+          value={val}
           strokeWidth={15}
           circleRatio={0.7}
           maxValue={3520}
@@ -33,7 +43,7 @@ function MyComponent() {
         <span className="minVal">0 A</span>
         <span className="maxVal left130">3520 A</span>
         <div className="circle">
-            <span className="value">640 A
+            <span className="value">{val} A
             <span className="textValue1"> Current Charger</span>
             <span className="textValue2"> Output</span></span>
         </div>
